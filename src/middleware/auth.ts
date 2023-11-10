51d56cc5c520c178911d146c70616e2789545e02
taflur <melhaouisoukaina1@gmail.com>
2023-11-10 16:38:00 +0100

    Implemented token authentication

---
+++ src/middleware/auth.ts
@@ -0,0 +1,34 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
+
+const secretKey = process.env.SECRET_KEY || '';
+
+export function generateToken(email: string): string {
+    return jwt.sign({ email }, secretKey, { expiresIn: '2h' });
+}
+
+export function authenticateToken(req: express.Request, res: express.Response, next: express.NextFunction) {
+    const token = req.header('Authorization');
+
+    if (!token) {
+        return res.status(401).json({error: 'No Authorization header found in the request'});
+    }
+
+    jwt.verify(token, secretKey, (err, user) => {
+        if (err) {
+            if (err) {
+                if (err.name === 'TokenExpiredError') {
+                    return res.status(401).json({error: 'Token has expired'});
+                } else {
+                    return res.status(403).json({error: 'Invalid token'});
+                }
+            }
+        }
+
+        req.body.email = (user as { email: string }).email;
+        next();
+    });
+}
